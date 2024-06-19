7bb7b2926e0c46b5fbc5f9c231f57aadcb225c3c
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-06-19 02:43:45 +0200

    fix port binding

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,7 @@ import auhRoutes from "./routes/auth";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING!);
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +28,6 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/api/users", userRoutes);
 app.use("/api/auth", auhRoutes);
 
-app.listen(7000, () => {
-  console.log("Server running on localhost:7000");
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
